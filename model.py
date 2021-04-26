ced0ae5c0b430fda7b9c21754260600185bb2aab
Amansingh1202 <aks001235@gmail.com>
2021-04-26 12:36:52 +0530

    added all reviews on display

--- model.py
+++ model.py
@@ -2,11 +2,14 @@ from flask import Flask, render_template, request
 import pandas as pd
 import requests
 import json
+import numpy as np
 
 url = "http://localhost:8501/v1/models/my_model:predict"
 
 
 app = Flask(__name__)
+
+reviews = []
 
 
 @app.route("/")
@@ -14,12 +17,10 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/sentiment", methods=["POST", "GET"])
+@app.route("/sentiment_analysis", methods=["POST", "GET"])
 def sentiment_analysis():
-    # model = load_model("model_py", compile=False)
-
     if request.method == "POST":
-        input = request.form.get("nm")
+        input = request.form.get("review")
         inputValue = [input]
         inputValues = pd.DataFrame(inputValue)
         print(inputValues)
@@ -32,16 +33,18 @@ def sentiment_analysis():
         print(data)
         headers = {"content-type": "application/json"}
         json_response = requests.post(url, data=data, headers=headers)
-        probability = json.loads(json_response.text)
+        output = json.loads(json_response.text)
+        probability = np.squeeze(output["predictions"][0])
         print(probability)
-        # probability = model.predict(np.array([input]))[0][0]
-        # print(probability)
-        # if probability < 0:
-        #     sentiment = "Negative"
-        # elif probability >= 0:
-        #     sentiment = "Positive"
+        if probability < 0:
+            review_result1 = 0
+        elif probability >= 0:
+            review_result1 = 1
+        new_review = {"reviewData": input, "result": review_result1}
+        reviews.append(new_review)
         return render_template(
-            "index.html", nm=input, probability=probability, sentiment=probability
+            "index.html",
+            reviews=reviews,
         )
 
 
